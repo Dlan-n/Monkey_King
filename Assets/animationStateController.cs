7576030affe28fb2a7322ce2a7c5588a42c6ed38
Dylan Shevlin <Dylanshevlinn@gmail.com>
2023-01-13 23:06:05 +0000

    Updated Player Movement Script

--- Assets/animationStateController.cs
+++ Assets/animationStateController.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class animationStateController : MonoBehaviour
 {
     Animator animator;
+    CharacterController characterController;
     int isWalkingHash;
     int isRunningHash;
+    public float speed;
+    public float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        
+        characterController = GetComponent<CharacterController>();
+
         //increases performance
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
@@ -21,6 +25,25 @@ public class animationStateController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //taking in the horizontal and vertical axis input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        //determines the direction of movement along the x and y axis
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection.Normalize();
+
+        characterController.SimpleMove(movementDirection * magnitude);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+
+
+
         bool isWalking = animator.GetBool(isWalkingHash);
         bool isRunning = animator.GetBool(isRunningHash);
         bool forwardPressed = Input.GetKey("w");
@@ -50,5 +73,6 @@ public class animationStateController : MonoBehaviour
             //set the isRunning boolean to be false
             animator.SetBool("isRunning", false);
         }
+        
     }
 }
