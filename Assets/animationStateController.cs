7fab231fff67c6549be757acd09c2d9e2df42562
Dylan Shevlin <Dylanshevlinn@gmail.com>
2023-01-13 21:56:38 +0000

    added running logic into the script

--- Assets/animationStateController.cs
+++ Assets/animationStateController.cs
@@ -6,18 +6,26 @@ public class animationStateController : MonoBehaviour
 {
     Animator animator;
     int isWalkingHash;
+    int isRunningHash;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        
+        //increases performance
         isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
     }
 
     // Update is called once per frame
     void Update()
     {
         bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
         bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
         //if player presses w key
         if(!isWalking && forwardPressed)
         {
@@ -29,6 +37,18 @@ public class animationStateController : MonoBehaviour
         {
             //then set the isWalking boolean to be true
             animator.SetBool("isWalking", false);
+        }
+        //if player is walking and not running and presses left shift
+        if (!isRunning && (forwardPressed && runPressed))
+        {
+            //set the isRunning boolean to be true
+            animator.SetBool("isRunning", true);
+        }
+        //if player is running and stops running or walking
+        if (isRunning && (!forwardPressed && !runPressed))
+        {
+            //set the isRunning boolean to be false
+            animator.SetBool("isRunning", false);
         }
     }
 }
