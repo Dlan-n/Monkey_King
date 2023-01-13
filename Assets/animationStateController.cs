94dba77fc0721b8a291f52f5d3ec1a87add5c38c
Dylan Shevlin <Dylanshevlinn@gmail.com>
2023-01-13 21:35:04 +0000

    Added script for walking forward and idle animations

--- Assets/animationStateController.cs
+++ Assets/animationStateController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class animationStateController : MonoBehaviour
 {
+    Animator animator;
+    int isWalkingHash;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+        //if player presses w key
+        if(!isWalking && forwardPressed)
+        {
+            //then set the isWalking boolean to be true
+            animator.SetBool("isWalking", true);
+        }
+        //if player is not pressing w key
+        if (isWalking && !forwardPressed)
+        {
+            //then set the isWalking boolean to be true
+            animator.SetBool("isWalking", false);
+        }
     }
 }
