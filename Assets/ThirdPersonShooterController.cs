e54e6c5daeedfa00a2a1d7de806333047a5ca641
Dylan Shevlin <Dylanshevlinn@gmail.com>
2023-01-16 15:53:35 +0000

    Added Main Menu

---
+++ Assets/ThirdPersonShooterController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ThirdPersonShooterController : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
