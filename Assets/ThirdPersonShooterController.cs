05a2e8b23c7bb837c5929de792a1d0c21d36acb8
Dylan Shevlin <Dylanshevlinn@gmail.com>
2023-01-16 22:55:48 +0000

    added player zoom

--- Assets/ThirdPersonShooterController.cs
+++ Assets/ThirdPersonShooterController.cs
@@ -2,14 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using StarterAssets;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
 
+    private StarterAssetsInputs starterAssetsInput;
+    private void Awake()
+    {
+        starterAssetsInput = GetComponent<StarterAssetsInputs>();
+    }
     // Update is called once per frame
     void Update()
     {
-
+        if(starterAssetsInput.aim)
+        {
+            aimVirtualCamera.gameObject.SetActive(true);
+        }
+        else
+        {
+            aimVirtualCamera.gameObject.SetActive(false);
+        }
     }
 }
